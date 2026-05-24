068d1ef1b4974a32f961635b91a14c75ea1f90d8
codewitharryy <aaryanawasthi444@gmail.com>
2026-05-25 04:08:15 +0530

    backend updates

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,9 +4,15 @@ const authRoute=require('./routes/auth.route')
 const chatRoute=require('./routes/chats.route')
 const cors=require('cors')
 const path=require('path')
-const app=express()
-//app middlewares
 
+
+//API routes
+
+app.use('/api/auth',authRoute)
+app.use('/api/chat',chatRoute)
+const app=express()
+
+//using middlewares
 app.use(cors({
     origin:"http://localhost:5173",
     methods:["GET","POST"],
@@ -17,10 +23,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname,'../public')))
 
-//API routes
 
-app.use('/api/auth',authRoute)
-app.use('/api/chat',chatRoute)
 
 app.get("*name",(req,res)=>{
     res.sendFile(path.join(__dirname,'../public/index.html'))
